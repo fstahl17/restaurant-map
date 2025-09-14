7dca5d7ba97324e6a86ba2022bd3a44ee6c65b1e
fstahl17 <93704519+fstahl17@users.noreply.github.com>
2025-09-14 18:17:35 +0200

    Updates

---
+++ _app/immutable/chunks/BDwQvRK9.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1mpx9q2?.base??"/sveltekit-github-pages",e=globalThis.__sveltekit_1mpx9q2?.assets??s;export{e as a,s as b};
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import Map from '$lib/main-map.svelte';
-</script>
-<Map/>
-
