65743c279e84c692471aaaf21e137954fc6533fb
fstahl17 <93704519+fstahl17@users.noreply.github.com>
2025-09-14 18:22:55 +0200

    Updates

--- _app/immutable/chunks/BDwQvRK9.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1mpx9q2?.base??"/sveltekit-github-pages",e=globalThis.__sveltekit_1mpx9q2?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/entry/start.DKAbU0SB.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Bb1sTatL.js";export{o as load_css,r as start};
